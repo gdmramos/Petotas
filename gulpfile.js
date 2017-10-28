fbd95473a4052c515f76876fff96e60adcf4ca2f
Gabriel <ramosdemiranda.gabriel@gmail.com>
2017-10-28 18:45:58 -0200

    Project Start

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+var stylus = require('gulp-stylus');
+
+// Servidor
+gulp.task('browser-sync',function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("./stylus/*.styl",['stylus']);
+    gulp.watch('./css/style.css').on("change", reload);
+    gulp.watch("./js/*.js").on("change", reload);
+    gulp.watch("./index.html").on("change", reload);
+});
+
+
+//Watch todos os .styl
+gulp.task('stylus', function(){
+    return gulp.src('./stylus/template.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./css/'));
+})
+
+
+gulp.task('default', ['browser-sync'], function(){
+   console.log("Gulp Iniciado");
+})
